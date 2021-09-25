c2e4e9262cc860e2a9425e584a77b3aa21123913
Victor Guerra Veloso <victorgvbh@gmail.com>
2021-09-25 19:08:57 -0300

    Added filter by query params

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.utils.datastructures import MultiValueDict
 from rest_framework import viewsets
 
 from app.models import Request, Response
@@ -9,10 +10,22 @@ class RequestViewSet(viewsets.ModelViewSet):
     serializer_class = RequestSerializer
     queryset = Request.objects.all()
 
+    def get_queryset(self):
+        kwargs = self.request.query_params.dict()
+        if "page" in kwargs:
+            kwargs.pop("page")
+        return self.queryset.filter(**kwargs)
+
 
 class ResponseViewSet(viewsets.ModelViewSet):
     serializer_class = ResponseSerializer
     queryset = Response.objects.all()
+
+    def get_queryset(self):
+        kwargs = self.request.query_params.dict()
+        if "page" in kwargs:
+            kwargs.pop("page")
+        return self.queryset.filter(**kwargs)
 
 
 def index(req):
