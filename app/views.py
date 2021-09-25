0e8abb82aa7d852c0b126210fa0d1446739106a5
Victor Guerra Veloso <victorgvbh@gmail.com>
2021-09-25 19:36:28 -0300

    Extract mixin + select only fields in query params

--- app/views.py
+++ app/views.py
@@ -1,31 +1,26 @@
 from django.http import HttpResponse
-from django.utils.datastructures import MultiValueDict
 from rest_framework import viewsets
 
 from app.models import Request, Response
 from app.serializers import RequestSerializer, ResponseSerializer
 
 
-class RequestViewSet(viewsets.ModelViewSet):
+class FieldsFilterMixin(viewsets.ModelViewSet):
+    def extract_fields(self, d: dict):
+        return {k: v for k, v in d.items() if k in self.serializer_class.Meta.model._meta.fields}
+
+    def get_queryset(self):
+        return self.queryset.filter(**self.extract_fields(self.request.query_params.dict()))
+
+
+class RequestViewSet(FieldsFilterMixin):
     serializer_class = RequestSerializer
     queryset = Request.objects.all()
 
-    def get_queryset(self):
-        kwargs = self.request.query_params.dict()
-        if "page" in kwargs:
-            kwargs.pop("page")
-        return self.queryset.filter(**kwargs)
 
-
-class ResponseViewSet(viewsets.ModelViewSet):
+class ResponseViewSet(FieldsFilterMixin):
     serializer_class = ResponseSerializer
     queryset = Response.objects.all()
-
-    def get_queryset(self):
-        kwargs = self.request.query_params.dict()
-        if "page" in kwargs:
-            kwargs.pop("page")
-        return self.queryset.filter(**kwargs)
 
 
 def index(req):
